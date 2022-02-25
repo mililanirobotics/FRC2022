f4f0e69550cd7f079a1bbd3b46ba80add6c83214
leesact <lctakara@gmail.com>
2022-02-24 22:00:04 -0800

    organized code

--- src/main/include/Robot.h
+++ src/main/include/Robot.h
@@ -50,16 +50,19 @@ class Robot : public frc::TimedRobot {
   double GetMedian(double value1, double value2, double value3);
   void DistanceToRPM(double distance);
   void limelightAlign();
+  void ScoringCargo();
 
+  //autonomous functions
   void encoderDrive(double speed, double leftInches, double rightInches, double timeoutSeconds);
   void horizontalConveyor(double speed, double rotations, double timeoutSeconds);
   void verticalConveyor(double speed, double rotations, double timeoutSeconds);
   void flywheel(double speed, double rotations, double timeoutSeconds);
   void turnDrive(double speed, double degrees, double timeoutSeconds);
 
- //CIMs  
-  TalonSRX C1Motor{10};
-  TalonSRX C2Motor{11};
+  //CIMs  
+  TalonSRX conveyorVerticalLeft{20};
+  TalonSRX conveyorVerticalRight{19};
+  TalonSRX horizontalConveyor{18};
 
   //pidcontroller  
   rev::SparkMaxPIDController testPIDController = flywheelShooter1.GetPIDController();
@@ -76,6 +79,7 @@ class Robot : public frc::TimedRobot {
   rev::CANSparkMax conveyorHorizontal {14, rev::CANSparkMax::MotorType::kBrushless};
   rev::CANSparkMax conveyorVertical {15, rev::CANSparkMax::MotorType::kBrushless};  
   rev::CANSparkMax flywheelShooter1 {16, rev::CANSparkMax::MotorType::kBrushless};  
+
   //follows flywheelShooter1
   rev::CANSparkMax flywheelShooter2 {17, rev::CANSparkMax::MotorType::kBrushless};
 
@@ -85,6 +89,14 @@ class Robot : public frc::TimedRobot {
   rev::SparkMaxRelativeEncoder verticalConveyorEncoder = conveyorVertical.GetEncoder();
   rev::SparkMaxRelativeEncoder horizontalConveyorEncoder = conveyorHorizontal.GetEncoder();
   rev::SparkMaxRelativeEncoder flywheelEncoder = flywheelShooter1.GetEncoder();
+
+  //Switch declarations
+  frc::DigitalInput horizontalSwitch{1}; //port will change 
+  frc::DigitalInput verticalSwitch{2}; //port will change
+
+  //solenoids
+  frc::DoubleSolenoid rightSolenoid{9, frc::PneumaticsModuleType::REVPH, 0, 1};
+  frc::DoubleSolenoid leftSolenoid{9, frc::PneumaticsModuleType::REVPH, 2, 3};
 
   //constant variables
   const double wheelDiameterInches = 453453453; //TBD
@@ -96,18 +108,24 @@ class Robot : public frc::TimedRobot {
   const double robotRadius = 2482304; //TBD
   const double degreesToInches = (degrees * (M_PI / 180) * robotRadius);
 
-  //Switch declarations
-  frc::DigitalInput horizontalSwitch{1};//port will change 
-  frc::DigitalInput verticalSwitch{2}; //port will change
+  //Limelight variables
+  double targetOffsetAngle_Horizontal;
+  double targetOffsetAngle_Vertical;
+  double targetArea;
+  double targetSkew;
 
-  //score cargo method
-  void ScoringCargo();
+  //angle to distance function variables:
+  double trueAngle;
+  double distanceToHub;
+  double firstRun, secondRun, thirdRun;
 
   //Gamepad assignments:
+
   //Controller 1
   double gamepad1_LStick = gamepad1.GetRawAxis(1);
   double gamepad1_RStick = gamepad1.GetRawAxis(5);
   double gamepad1_RTrigger = gamepad1.GetRawAxis(3);
+
   //Controller 2
   bool gamepad2_YButton = gamepad2.GetRawButtonPressed(4);
   bool gamepad2_XButton = gamepad2.GetRawButtonPressed(3);
@@ -115,17 +133,7 @@ class Robot : public frc::TimedRobot {
   double gamepad2_LTrigger = gamepad2.GetRawAxis(2);
   double gamepad2_RTrigger = gamepad2.GetRawAxis(3);
 
-  //Limelight variables
-  double targetOffsetAngle_Horizontal;
-  double targetOffsetAngle_Vertical;
-  double targetArea;
-  double targetSkew;
-  
-  //angle to distance function variables:
-  double trueAngle;
-  double distanceToHub;
-  
-  double firstRun, secondRun, thirdRun;
+  //PID
 
   //kP = 0.000001, kFF 0.0001695
   //Change the feed forward to adjust settling below or above the set point
